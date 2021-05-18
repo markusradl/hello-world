338b310c58e0cdc3e5814b4ba5955fecadcbb1d2
Markus Radl <markus@markusradl.com>
2021-05-18 23:08:54 +0200

    Conditional rendering of html
    Between the beginning #if an the /if for ending we can render
    the html code in Between
    :else or :else if inserts additional blocks

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,20 @@
 <script>
-	const name = 'Maxx'
-	const channel = '<b>mrsln<b>'
-	const hack =`<a href="#" onclick="alert('You have been hacked')">Win a prize<a>`
-	const headingID = 'heading'
-	const id = 'heading1'
-	const disabled = true
-	const status = 'success'
-	const isPromoted = false
-	const promoted = true
+	const num = 'Stringggg'
 </script>
 
 <main>
-	<h2 class="underline">Underlined Text</h2>
-	<h2 class="{status}">Status</h2>
-	<h2 class="{isPromoted ? 'promoted' : ''}">Movie Title - Contionial class</h2>
-	<h2 class:promoted={isPromoted}>Conditional binding short hand</h2>
-	<h2 class:promoted>Shorter hand, if variable and class has the same name</h2>
-	<h1>Hello {2 * 3 + name}</h1>
-	<div>{@html channel}</div>
-	<div>{@html hack}</div>
-	<h2 id={headingID}>This is a Heading with id-tag heading</h2>
-	<h2 {id}>This is the same tag as above with dynamic id - short hand notation</h2>
-	<button disabled={disabled}>Binding short hand notation</button>
-		
+	{#if num === 0}
+		<h2>The number is Zero</h2>
+	{:else if num < 0}
+		<h2>The number is negativ</h2>
+		{:else if num > 0}
+	<h2>The number is positive</h2>
+	{:else}
+		<h2>The number is Not a Number</h2>
+	{/if}
 </main>
 
 <style>
-	.underline {
-		text-decoration: underline;
-	}
-
-	.danger {
-		color: #ff3e00;
-	}
-
-	.success {
-		color: darkgreen;
-	}	
-
-	.promoted {
-		font-style: italic;
-	}
 
 	main {
 		text-align: center;
