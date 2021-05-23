8f7efea12cc5e3286d5c983948b41c5cd6857134
Markus Radl <markus@markusradl.com>
2021-05-23 17:14:37 +0200

    Form handling with different input types

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,133 @@
 <script>
-	let count = 0
+	const formValues ={
+		name: '',
+		profileSummary: '',
+		country: '',
+		jobLocation: [],
+		remoteWork: false,
+		skillSet: [],
+		yearsOfExperience: ''
+	}
 
-	function handleClick(event, stepSize) {
-		count += stepSize
-		console.log(event)
+	function submitForm(event) {
+		console.log(formValues)
 	}
 </script>
 
 <main>
-	<!-- In Line Event handler -->
-	<button on:click={() => (count = count + 1)} >Count up! Value: {count}</button>
-  
-	<button on:click={(event) => handleClick(event, 5)} >Count up by 5! Value: {count}</button>
-	<button on:click={(event) => handleClick(event, 10)} >Count up by 10! Value: {count}</button>
+	<div>
+		<pre>
+			{JSON.stringify(formValues, null, 2)}
+		</pre>
+	</div>
+	<form on:submit|preventDefault={submitForm}>
+		<div>
+			<label for="name">Vorname</label>
+			<!--Svelte casts the datatype accordingly to input type-->
+			<input type="number" id="name" bind:value={formValues.name}>
+		</div>
 
+		<div>
+			<label for="profile">Profile Summary</label>
+			<textarea id="profile" bind:value={formValues.profileSummary} placeholder="Please enter some text"></textarea>
+		</div>
+
+		<div>
+			<label for="country">Country</label>
+			<select id="country" bind:value={formValues.country}>
+				<option value="">Select a country</option>
+				<option value="india">India</option>
+				<option value="austria">Austria</option>
+				<option value="england">England</option>
+			</select>
+		</div>
+
+		<div>
+			<label for="job-location">Job Location</label>
+			<select 
+				id="job-location" 
+				bind:value={formValues.jobLocation}
+				multiple
+			>
+				<option value="">Select a country</option>
+				<option value="india">India</option>
+				<option value="austria">Austria</option>
+				<option value="england">England</option>
+			</select>
+		</div>
+
+		<div>
+			<input
+				type="checkbox"
+				id="remoteWork"
+				bind:checked={formValues.remoteWork}
+			/>
+			<label for="remoteWork">Open for remote work?</label>
+		</div>
+
+		<div>
+			<label>Skill set</label>
+				<input
+					type="checkbox"
+					id="css"
+					value="css"
+					bind:group={formValues.skillSet}
+				/>
+				<label for="css">CSS</label>
+				<input
+					type="checkbox"
+					id="html"
+					value="html"
+					bind:group={formValues.skillSet}
+				/>
+				<label for="css">HTML</label>
+				<input
+					type="checkbox"
+					id="javascript"
+					value="javascript"
+					bind:group={formValues.skillSet}
+				/>
+				<label for="javascript">JavaScript</label>
+		</div>
+
+		<div>
+			<label>Years of Experience</label>
+				<input
+					type="radio"
+					id="1-2"
+					value="1-2"
+					bind:group={formValues.yearsOfExperience}
+				/>
+				<label for="1-2">1-2</label>
+				<input
+					type="radio"
+					id="3-5"
+					value="3-5"
+					bind:group={formValues.yearsOfExperience}
+				/>
+				<label for="3-5">3-5</label>
+				<input
+					type="radio"
+					id="5-10"
+					value="5-10"
+					bind:group={formValues.yearsOfExperience}
+				/>
+				<label for="5-10">5-10</label>
+		</div>
+
+		<div>
+			<button>Submit</button>
+		</div>
+	</form>
 </main>
 
 <style>
+	input + label {
+		display: inline-flex;
+	}
 
 	main {
-		text-align: center;
+		/*text-align: center;*/
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
